7470aca80b1a795fa8d8f9d2d2473a91f75b84a0
rgarro <rgarro@gmail.com>
2024-04-11 07:34:27 -0600

    genius mouse usb ..

--- Assets/Scripts/showHideRoof.cs
+++ Assets/Scripts/showHideRoof.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class showHideRoof : MonoBehaviour
 {
+
+    public GameObject roofMetalSheetA;
+    public GameObject roofMetalSheetB;
+    public GameObject roofMetalSheetC;
+    public GameObject roofMetalSheetD;
+    private Boolean roof_is_shown;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    void showRoof(){
         
     }
 
